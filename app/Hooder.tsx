46369aa7ccdb2c2ce7d2926f8d9e90445665dd59
RAO FAHAD <raofahad046@gmail.com>
2023-01-22 21:05:23 +0500

    Chakra Ui Installed and Successfully added to the Project

---
+++ app/Hooder.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Box, Flex, Text, Heading } from "@chakra-ui/react";
+
+function Hooder() {
+  return (
+    <Flex direction="column" alignItems="center">
+      <Heading>Simple pricing for your business</Heading>
+      <Text>Plans that are carefully crafted to suit your business.</Text>
+    </Flex>
+  );
+}
+
+export default Hooder;
